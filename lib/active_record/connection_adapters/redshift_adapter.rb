10120b3af16643340429ce261c09c2d3991062a3
Ben Murphy <benmmurphy@gmail.com>
2015-09-04 12:56:26 +0100

    fix query for redshift in rails 4.2.4

--- lib/active_record/connection_adapters/redshift_adapter.rb
+++ lib/active_record/connection_adapters/redshift_adapter.rb
@@ -45,3 +45,23 @@ module ActiveRecord
   end
 end
 
+module ActiveRecord
+  module ConnectionAdapters
+    module PostgreSQL
+      module OID
+        def query_conditions_for_initial_load(type_map)
+          known_type_names = type_map.keys.map { |n| "'#{n}'" }
+          known_type_types = %w('r' 'e' 'd')
+          <<-SQL % [known_type_names.join(", "), known_type_types.join(", ")]
+            WHERE
+              t.typname IN (%s)
+              OR t.typtype IN (%s)
+              OR t.typinput = 'array_in'::regproc
+              OR t.typelem != 0
+          SQL
+        end
+      end
+    end
+  end
+end
+
